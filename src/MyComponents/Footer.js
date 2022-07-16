629f7f90f2264d8dabcfe32e110d9afa492ee715
AksharSans <sanyalamitrakshar@gmail.com>
2022-07-16 22:00:24 +0530

    Added Footer

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer fucking works!!</div>
+    <div className="bg-dark text-light py-2 my-3">
+      <p className="text-center">Copyright &copy; Amitrakshar</p>
+    </div>
   )
 }
