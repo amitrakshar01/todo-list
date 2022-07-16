9cb8492627710db0f69f1dc29af27eaaffd1c319
AksharSans <sanyalamitrakshar@gmail.com>
2022-07-16 21:57:13 +0530

    ..

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -5,10 +5,11 @@ export const Todos = (props) => {
   return (
     <div className="container">
       <h2 className="text-center">My Todos List</h2>
-      
-      {props.todos.map((todo) => {
+      {props.todos.length===0?<h4 className="text-center">No Todos</h4>:props.todos.map((todo) => {
         return <TodoItem todo={todo} onDelete={props.onDelete}/>
       })}
+      
+      
     </div>
   );
 }
